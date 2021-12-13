62b4e284c5f9c8f29761ed03f2d35d785f7319ae
donggunkwak <47698773+donggunkwak@users.noreply.github.com>
2021-12-13 08:59:41 -0800

    Added button color change

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
+var userColors = [];
 
-
-
-document.getElementById("redButton").addEventListener("click", function() {
+function redButtonClicked(){
   console.log('Red button clicked');
+  userColors.push('R');
+  console.log(userColors);
   document.getElementById("redButton").classList.add("shadow");
   document.getElementById("redButton").classList.remove("red");
-  //setTimeout(document.getElementById("redButton").classList.remove("shadow"), 1000);
-});
+  setTimeout(()=>{
+  document.getElementById("redButton").classList.remove("shadow");
+  document.getElementById("redButton").classList.add("red");
+  }, 500);
+}
+
+
+document.getElementById("redButton").addEventListener("click", redButtonClicked);
